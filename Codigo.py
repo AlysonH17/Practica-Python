42049b8d62764e03046e1b684252758e141575f7
AlysonH17 <alyson.uni2@gmail.com>
2024-11-22 07:58:52 -0500

    Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -120,9 +120,9 @@ def generar_analisis():
 
     # Generar AST
     arbol = arbol.copy()
-    for subtree in arbol.subtrees():
-        if subtree.label() not in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            subtree.set_label('')  
+    for subarbol in arbol.subtrees():
+        if subarbol.label() not in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            subarbol.set_label('')  
     arbol.draw()
 
 # Crear la interfaz gráfica
